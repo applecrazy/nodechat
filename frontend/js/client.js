64d9f7fa8f9f25a04e6b85f8ae8a251c76457071
applecrazy <applecrazy@users.noreply.github.com>
2016-09-25 20:39:42 -0700

    Add cool features - Offline notices - Tighter server integration  - Admin messages - Settings variable - Log time - Mobile styles (WIP) - IP logging

--- frontend/js/client.js
+++ frontend/js/client.js
@@ -1,40 +1,101 @@
 var NodeChat = {
 	nickname: null,
-	send: function() {
-		var mm = $('.chatinput').val();
-		var badinput =/<(.|\n)*?>/g;
-		if (badinput.test(mm)) {alert("Nice one :).\nBut seriously, no HTML injection, man."); return}
-		$('.container').append(`
-			<div class="msg me">
-				<span class="user">${NodeChat.nickname}</span>
-				&emsp;
-				<span class="msgtxt">${mm}</span>
-				&emsp;
-			</div>`);
+	resp: null,
+	offline: false,
+	send: function () {
+		var mm = $('.chatinput')
+			.val()
+			.trim();
+		if (mm == "") return;
+		var badinput = /<(.|\n)*?>/g;
+		if (badinput.test(mm)) { alert("Nice one :).\nBut seriously, no HTML injection."); return }
+		var newms = {
+			username: NodeChat.nickname,
+			message: mm
+		};
+		$.post('/send', newms, function (data) {
+			if (data) {
+				alert("MALFORMED MESSAGE!");
+			}
+		});
+		NodeChat.getMessages();
 		$('.chatinput').val('');
 		NodeChat.goBottom();
 	},
-	randomNickname: function() {
+	randomNickname: function () {
 		if (this.nickname) return;
-	    var nick = "";
-	    var ltrs = "!abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz";
+		var nick = "";
+		var ltrs = "!abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz";
 
-	    for(var i = 0; i < 5; i++)
-	        nick += ltrs.charAt(Math.floor(Math.random() * ltrs.length));
+		for (var i = 0; i < 5; i++)
+			nick += ltrs.charAt(Math.floor(Math.random() * ltrs.length));
 
-	    nick += Math.floor(Math.random());
-	    this.nickname = nick;
-	    // alert('Your nickname is "'  + this.nickname + '".');
+		nick += Math.floor(Math.random());
+		this.nickname = nick;
+		// alert('Your nickname is "'  + this.nickname + '".');
 	},
-	goBottom: function() {
+	goBottom: function () {
 		$('.container').animate({ scrollTop: $('.container').height() }, "slow");
+	},
+	getMessages: function () {
+		$.ajaxSetup({ cache: false });
+		$.getJSON("/msgs", function (data) {
+			// console.log(data);
+			NodeChat.resp = data;
+			if (NodeChat.offline) {NodeChat.offline = false;$('.chatinput').prop('disabled', false);$('.chatinput').prop('placeholder', 'send message by pressing Enter');}
+		})
+			.then(function () {
+				// console.log("Propagating changes...");
+				NodeChat.update();
+			})
+			.fail(function () {
+				if (!NodeChat.offline) {
+					$('.container').append(`
+					<div class="msg">
+						<span class="user">meow <small style=\"font-size: 2vw\">[ADMIN]</small></span>
+						&emsp;
+						<span class="msgtxt"><small>Hey there! It looks like the NodeChat server is down or you have disconnected from the Internet.</small></span>
+						&emsp;
+					</div>`);
+					NodeChat.offline = true;
+					$('.chatinput').prop('disabled', true);
+					$('.chatinput').prop('placeholder', 'you\'re offline...');
+				}
+				NodeChat.goBottom();
+			})
+			;
+
+	},
+	update: function () {
+		// // console.log(NodeChat.resp);
+		if (!NodeChat.resp) { return }
+		if ('messages' in NodeChat.resp) {
+			$('.container').html('');
+			$.each(NodeChat.resp.messages, function (i, val) {
+				var style = "msg";
+				if (val.username == NodeChat.nickname) { style += " me"; }
+				$('.container').append(`
+					<div class="${style}">
+						<span class="user">${val.username}</span>
+						&emsp;
+						<span class="msgtxt">${val.message}</span>
+						&emsp;
+					</div>`);
+			});
+		}
+		NodeChat.goBottom();
+		return;
 	}
 };
 
 
 
-$(function() {
+$(function () {
+	$.ajaxSetup({ cache: false });
 	NodeChat.randomNickname();
 	$('.chatinput').change(NodeChat.send);
 	$('.chatinput')[0].focus();
+	// NodeChat.getMessages();
+	// NodeChat.update();
+	var checker = setInterval(NodeChat.getMessages, 1000);
 });
--- server.js
+++ server.js
@@ -1,43 +1,63 @@
 // server.js
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('frontend'));
 
+app.use(bodyParser.urlencoded({extended : true}));
+
+const $SETTINGS = {
+	MESSAGE_CACHE_LIMIT: 10,
+	RUNNING_PORT: 8080,
+	WELCOME_MESSAGE: {username: "meow <small style=\"font-size: 2vw\">[ADMIN]</small>", message: "<small>Welcome!</small>"},
+	MOD_MESSAGE: {username: "meow <small style=\"font-size: 2vw\">[ADMIN]</small>", message: "<small>This is moderated by cats + humans.</small>"}
+};
 
 
+var CurrentMsgs = {
+	status: "OK",
+	timestamp: null,
+	messages: [$SETTINGS.WELCOME_MESSAGE, $SETTINGS.MOD_MESSAGE]
+};
+function preventOverflow() {
+	if (CurrentMsgs.messages.length >= $SETTINGS.MESSAGE_CACHE_LIMIT) {
+		CurrentMsgs.messages = CurrentMsgs.messages.slice(Math.max(CurrentMsgs.messages.length - $SETTINGS.MESSAGE_CACHE_LIMIT, 1));
+	}
+}
+function logOutput(msg) {
+	console.log(`[${new Date(new Date().getTime()).toLocaleString()}] ${msg}`);
+}
 
-
-/* 
-
-BOILERPLATE BELOW...
-
-*/
-
-// http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (request, response) {
-  response.sendFile(__dirname + '/frontend/index.html');
+app.get("/msgs", function (req, res) {
+	// var resp = "{";
+	// for (var i = 0; i < CurrentMsgs.length; i++) {
+	// 	resp += JSON.stringify(CurrentMsgs[i]);
+	// }
+	// resp += "}";
+	logOutput("GET /msgs");
+	preventOverflow();
+	CurrentMsgs.timestamp = Math.floor(Date.now() / 1000);
+	res.header("Content-Type", "application/json");
+	res.json(CurrentMsgs);
 });
 
-app.get("/dreams", function (request, response) {
-  response.send(dreams);
-});
-
-// could also use the POST body instead of query string: http://expressjs.com/en/api.html#req.body
-app.post("/dreams", function (request, response) {
-  dreams.push(request.query.dream);
-  response.sendStatus(200);
-});
-
-// Simple in-memory store for now
-var dreams = [
-  "Find and count some sheep",
-  "Climb a really tall mountain",
-  "Wash the dishes"
-  ];
-
-// listen for requests :)
-var listener = app.listen(8080, function () {
-  console.log('Server is running on port ' + listener.address().port);
+app.post("/send", function(req, res) {
+	var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+	logOutput('POST /send ' + ip);
+	preventOverflow();
+	if ('username' in req.body && 'message' in req.body) {
+		CurrentMsgs.messages.push(req.body);
+		// logOutput(CurrentMsgs.messages);
+	} else {
+		res.send("MALFORMED_MESSAGE");
+	}
+	res.end();
+}); 
+// listen for connections
+var server = app.listen($SETTINGS.RUNNING_PORT, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+  console.log(`Server is running on ${host}:${port}...`);
 });
